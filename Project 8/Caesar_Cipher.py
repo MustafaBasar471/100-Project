6f635da291b0b3d8a5700b9a3d53b1dcf42b0a1f
MustafaBasar471 <mustafabasar471@gmail.com>
2022-05-16 00:52:13 +0300

    Caesar_Cipher added

---
+++ Project 8/Caesar_Cipher.py
@@ -0,0 +1,56 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+            'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+def caesar(s_text, shift_value, cipher_direction):
+    e_text = ""
+    if cipher_direction == "decode":
+            shift_value *= -1
+    for i in s_text:
+        if i in alphabet:   
+            position = alphabet.index(i)
+            new_position = position + shift_value
+            e_text += alphabet[new_position]
+        else:
+            e_text += i
+    print(f"The {cipher_direction}d text is {e_text}")
+sh_continue = True
+while sh_continue:
+    direction = input("Type 'encode' to Encrypt, type 'decode' to decrypt:\n")
+    txt = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    shift = shift % 26
+    caesar(s_text=txt, shift_value=shift, cipher_direction=direction)
+    restart = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n").lower()
+    if restart == "no":
+        sh_continue = True
+        print("Goodbye")
+        break
+
+
+
+
+# My Code
+
+# def encrypt(txt_text, shift_value):
+#     cipher_text = ""
+#     for i in txt_text:
+#         position = alphabet.index(i)
+#         new_position = position + shift_value
+#         cipher_text += alphabet[new_position]
+#     print(f"The encoded text is {cipher_text}")
+
+
+# def decrypt(txt_text, shift_value):
+#     cipher_text = ""
+#     for i in txt_text:
+#         position = alphabet.index(i)
+#         new_position = position - shift_value
+#         cipher_text += alphabet[new_position]
+#     print(f"The decoded text is {cipher_text}")
+
+# if direction == 'encode':
+#     encrypt(txt_text=txt, shift_value=shift)
+# elif direction == 'decode':
+#     decrypt(txt_text=txt, shift_value=shift)
+# else:
+#     print("Make sure you spelled correctly !")
